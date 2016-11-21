1b42da57a74024854af3b9729149f1fc91e7adcd
waltonam <waltonam@g.cofc.edu>
2016-11-21 14:40:48 -0500

    added ballot and tested gitignore

---
+++ src/client/Ballot.java
@@ -0,0 +1,49 @@
+package client;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hoisi on 10/27/2016.
+ */
+public class Ballot {
+    private ArrayList candidates = new ArrayList();
+
+    boolean ballotComplete;
+    int voterID;
+
+    public Ballot(ArrayList x){
+        //used to be this, but I think this was just a reference. Need a new object
+//        this.candidates = x;
+        for(int j = 0; j < x.size(); j++) {
+            Candidate add = new Candidate();
+            Candidate y = (Candidate) x.get(j);
+            add.setBio(y.getBio());
+            add.setName(y.getName());
+            add.setParty(y.getParty());
+            this.candidates.add(add);
+        }
+    }
+
+    public void print(){}
+
+    public boolean isBallotComplete() {
+        return ballotComplete;
+    }
+
+    public void setBallotComplete(boolean ballotComplete) {
+        this.ballotComplete = ballotComplete;
+    }
+
+    public int getVoterID() {
+        return voterID;
+    }
+
+    public void setVoterID(int voterID) {
+        this.voterID = voterID;
+    }
+
+    public ArrayList getCandidates() {
+        return candidates;
+    }
+
+}
